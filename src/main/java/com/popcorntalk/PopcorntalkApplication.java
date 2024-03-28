47c04f29ce63d10bbd7337050e4726c5ca2d8ceb
wooseok <wooseok6008@gmail.com>
2024-03-28 11:09:15 +0900

    initial commit

---
+++ src/main/java/com/popcorntalk/PopcorntalkApplication.java
@@ -0,0 +1,13 @@
+package com.popcorntalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopcorntalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopcorntalkApplication.class, args);
+    }
+
+}
