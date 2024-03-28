8b10ebafdfb197a0020a7628bd443d335148a274
parkjungsub <eses565@naver.com>
2024-03-29 01:42:06 +0900

    feat(product): PutMapping 추가

--- src/main/java/com/popcorntalk/domain/product/controller/ProductController.java
+++ src/main/java/com/popcorntalk/domain/product/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.popcorntalk.domain.product.controller;
 
 import com.popcorntalk.domain.product.dto.ProductCreateRequestDto;
+import com.popcorntalk.domain.product.dto.ProductUpdateRequestDto;
 import com.popcorntalk.domain.product.service.ProductService;
 import com.popcorntalk.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +39,17 @@ public class ProductController {
         @PathVariable Long productId,
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
         productService.deleteProduct(productId, userDetails.getUser());
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    //상품 수정
+    @PutMapping("/{productId}")
+    public ResponseEntity<Void> updateProduct(
+        @PathVariable Long productId,
+        @RequestBody ProductUpdateRequestDto productUpdateRequestDto,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        productService.updateProduct(productId, productUpdateRequestDto, userDetails.getUser());
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
