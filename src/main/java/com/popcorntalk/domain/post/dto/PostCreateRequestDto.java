0caefce300d57c6ebe6fccba49b563c554a40b39
pshistory <psh_a@naver.com>
2024-03-28 20:04:37 +0900

    fix(post) : PostCreateRequestDto Vaildation 설정
    필드모두 @NotEmpty로 설정

--- src/main/java/com/popcorntalk/domain/post/dto/PostCreateRequestDto.java
+++ src/main/java/com/popcorntalk/domain/post/dto/PostCreateRequestDto.java
@@ -1,11 +1,15 @@
 package com.popcorntalk.domain.post.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 @Getter
 public class PostCreateRequestDto {
 
+    @NotEmpty
     private String postName;
+    @NotEmpty
     private String postContent;
+    @NotEmpty
     private String postImage;
 }
