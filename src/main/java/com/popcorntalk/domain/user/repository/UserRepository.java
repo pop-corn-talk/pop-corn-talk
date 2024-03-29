21aea47793405f510365d4a114e30b28d6f011a8
harrisbang2 <harrisbang2@gmail.com>
2024-03-29 18:14:05 +0900

    fix(user) : 유저 레포지토리 복원

--- src/main/java/com/popcorntalk/domain/user/repository/UserRepository.java
+++ src/main/java/com/popcorntalk/domain/user/repository/UserRepository.java
@@ -3,9 +3,8 @@ package com.popcorntalk.domain.user.repository;
 import com.popcorntalk.domain.user.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-//
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    //todo 쿼리 dsl 마춰서 복원 작업 해야 합니다
     //boolean existsByEmail(String email);
 
     Optional<User> findByEmail(String email);
