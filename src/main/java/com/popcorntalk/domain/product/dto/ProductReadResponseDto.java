4be3cb2795136307aa3af3f1a000a6d5b5f33a5b
parkjungsub <eses565@naver.com>
2024-04-01 20:30:39 +0900

    fix(product) : 필요없는 생성자 및 메소드 삭제

--- src/main/java/com/popcorntalk/domain/product/dto/ProductReadResponseDto.java
+++ src/main/java/com/popcorntalk/domain/product/dto/ProductReadResponseDto.java
@@ -19,17 +19,4 @@ public class ProductReadResponseDto {
     private LocalDateTime createAt;
     private LocalDateTime modifiedAt;
 
-    public ProductReadResponseDto(Product product) {
-        this.id = product.getId();
-        this.name = product.getName();
-        this.image = product.getImage();
-        this.description = product.getDescription();
-        this.price = product.getPrice();
-        this.createAt = product.getCreatedAt();
-        this.modifiedAt = product.getModifiedAt();
-    }
-//
-//    public static ProductReadResponseDto createOf(Product product){
-//        return new ProductReadResponseDto(product);
-//    }
 }
