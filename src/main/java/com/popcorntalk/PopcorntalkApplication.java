63c85fcf7634c1e995f0ca3ae8b2ba71ef656e03
wooseok <wooseok6008@gmail.com>
2024-04-01 17:59:36 +0900

    fix(merge) dev 충돌 해결

--- src/main/java/com/popcorntalk/PopcorntalkApplication.java
+++ src/main/java/com/popcorntalk/PopcorntalkApplication.java
@@ -4,15 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
-@EnableJpaAuditing
 @SpringBootApplication
 public class PopcorntalkApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PopcorntalkApplication.class, args);
     }
-
 }
 
 
