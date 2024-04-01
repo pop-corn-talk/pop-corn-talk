8d4baada19c648bd35446a7fcf17b6faf3e21cfd
harrisbang2 <harrisbang2@gmail.com>
2024-04-01 20:49:12 +0900

    refactor(user) : 주석 변경

--- src/main/java/com/popcorntalk/domain/user/service/UserService.java
+++ src/main/java/com/popcorntalk/domain/user/service/UserService.java
@@ -3,19 +3,41 @@ package com.popcorntalk.domain.user.service;
 import com.popcorntalk.domain.user.dto.UserInfoResponseDto;
 import com.popcorntalk.domain.user.dto.UserPublicInfoResponseDto;
 import com.popcorntalk.domain.user.dto.UserSignupRequestDto;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface UserService {
-    //회원가입
+    /**
+     * 회원가입
+     *
+     * @param userSignupRequestDto 생성될 유저의 내용
+     *
+     */
     void signup(UserSignupRequestDto userSignupRequestDto);
 
-    // 유저가 자신 개인의 정보를 조회. email 포인트 포스트 횟수
+
+    /**
+     * 개인(본인) 정보 조회
+     *
+     * @param userId 조회할 유저의 내용
+     *
+     */
     UserInfoResponseDto getMyInfo(Long userId);
 
-    // 유저가 다른 이의 정보를 확인. email 만
+    /**
+     * 개인(타인) 정보 조회
+     *
+     * @param userId 조회할 유저의 내용
+     *
+     */
     UserPublicInfoResponseDto getUserInfo(Long userId);
 
-    // 유저가 다른 이들의 정보를 확인. email 만
+    /**
+     * 개인(타인) 정보 페이지 로 조회
+     *
+     * @param pageable 조회할 페이지 요청
+     *
+     */
     Page<UserPublicInfoResponseDto> getAllUserInfo(Pageable pageable);
 }
