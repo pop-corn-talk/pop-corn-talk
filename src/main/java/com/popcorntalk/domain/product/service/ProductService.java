9dffc0837ee1ba669dde557ccf012d79f415e0b8
parkjungsub <eses565@naver.com>
2024-04-01 11:47:02 +0900

    fix(product) : createProduct, deleteProduct, updateProduct 매개변수 값 UserRole -> userid로 변경

--- src/main/java/com/popcorntalk/domain/product/service/ProductService.java
+++ src/main/java/com/popcorntalk/domain/product/service/ProductService.java
@@ -3,18 +3,17 @@ package com.popcorntalk.domain.product.service;
 import com.popcorntalk.domain.product.dto.ProductCreateRequestDto;
 import com.popcorntalk.domain.product.dto.ProductReadResponseDto;
 import com.popcorntalk.domain.product.dto.ProductUpdateRequestDto;
-import com.popcorntalk.domain.user.entity.UserRoleEnum;
 import java.util.List;
 
 public interface ProductService {
 
     void createProduct(ProductCreateRequestDto productCreateRequestDto,
-        UserRoleEnum userRoleEnum);
+        Long userId);
 
-    void deleteProduct(Long productId, UserRoleEnum userRoleEnum);
+    void deleteProduct(Long productId, Long userId);
 
     void updateProduct(Long productId, ProductUpdateRequestDto productUpdateRequestDto,
-        UserRoleEnum userRoleEnum);
+        Long userId);
 
     List<ProductReadResponseDto> getProduct();
 }
