e3e966d4fda4ae91637517681bd4500c47cb226b
onenyeong <a48142826@gmail.com>
2024-04-01 17:23:52 +0900

    refactor(Comment) : 서비스에 주석추가

--- src/main/java/com/popcorntalk/domain/comment/service/CommentService.java
+++ src/main/java/com/popcorntalk/domain/comment/service/CommentService.java
@@ -9,14 +9,39 @@ import org.springframework.data.domain.Pageable;
 
 public interface CommentService {
 
-
+    /**
+     *
+     * @param userId 유저의 존재여부를 확인하기위한 유저의 번호
+     * @param postId 게시글의 존재여부를 확인하기 위한 게시글의 번호
+     * @param requestDto 생성될 댓글의 내용
+     */
     void createComment(Long userId, Long postId,
         CommentCreateRequestDto requestDto);
 
+    /**
+     *
+     * @param userId 유저의 존재여부를 확인하기위한 유저의 번호
+     * @param postId 게시글의 존재여부를 확인하기 위한 게시글의 번호
+     * @param commentId 수정할 댓글의 번호
+     * @param requestDto 수정될 댓글의 내용
+     */
     void updateComment(Long userId, Long postId, Long commentId,
         CommentUpdateRequestDto requestDto);
 
+    /**
+     *
+     * @param userId 유저의 존재여부를 확인하기위한 유저의 번호
+     * @param postId 게시글의 존재여부를 확인하기 위한 게시글의 번호
+     * @param commentId 삭제할 댓글의 번호
+     */
     void deleteComment(Long userId, Long postId, Long commentId);
 
+    /**
+     *
+     * @param userId 유저의 존재여부를 확인하기위한 유저의 번호
+     * @param postId 게시글의 존재여부를 확인하기 위한 게시글의 번호
+     * @param pageable 페이징 처리(size=10,page=0)
+     * @return Page<CommentGetResponseDto>
+     */
     Page<CommentGetResponseDto> getComments(Long userId, Long postId, Pageable pageable);
 }
