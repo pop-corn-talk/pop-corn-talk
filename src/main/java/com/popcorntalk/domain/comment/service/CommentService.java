bac004b20068b7cdaaef1ee64c3eb8f3a04f8d17
onenyeong <a48142826@gmail.com>
2024-04-01 16:09:50 +0900

    refactor(Comment) : interface에 작성된 transactional 제거

--- src/main/java/com/popcorntalk/domain/comment/service/CommentService.java
+++ src/main/java/com/popcorntalk/domain/comment/service/CommentService.java
@@ -5,10 +5,8 @@ import com.popcorntalk.domain.comment.dto.CommentGetResponseDto;
 import com.popcorntalk.domain.comment.dto.CommentUpdateRequestDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.transaction.annotation.Transactional;
 
 
-@Transactional
 public interface CommentService {
 
 
