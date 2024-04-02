2a7207f4872a6b6027e0f0fe5bf9b13f3b91ab6a
harrisbang2 <harrisbang2@gmail.com>
2024-04-02 10:43:45 +0900

    test(user) : 해당 유저가 관리자인지 확인 하는 메소드 테스트

---
+++ src/test/java/com/popcorntalk/User/UserServiceImplTest.java
@@ -0,0 +1,74 @@
+package com.popcorntalk.User;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.util.AssertionErrors.assertFalse;
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
+import com.popcorntalk.domain.user.entity.User;
+import com.popcorntalk.domain.user.entity.UserRoleEnum;
+import com.popcorntalk.domain.user.repository.UserRepository;
+import com.popcorntalk.domain.user.repository.UserRepositoryCustom;
+import com.popcorntalk.domain.user.repository.UserRepositoryCustomImpl;
+import com.popcorntalk.domain.user.service.UserServiceImpl;
+import com.popcorntalk.global.exception.customException.UserNotFoundException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class UserServiceImplTest {
+  @Mock
+  UserRepository mockRepo;
+
+  @Mock
+  PasswordEncoder passwordEncoder;
+
+
+  @Test
+  @DisplayName("관리자 유저일 경우")
+  void validateAdminSuccess(){
+    //given
+    long userId = 1;
+    boolean flag = true;
+    UserServiceImpl userService = new UserServiceImpl(mockRepo,passwordEncoder);
+    given(mockRepo.validateAdminUser(userId)).willReturn(true);
+//    given(mockRepoCus.validateAdminUser(userId)).willReturn(true);
+//    given(mockRepoCusImpl.validateAdminUser(userId)).willReturn(true);
+
+    //when
+    try {
+      userService.validateAdminUser(userId);
+    }catch (UserNotFoundException e){
+      flag = false;
+    }
+    // then
+    assertTrue("성공 해당 유저는 관리자가 맞아요",flag);
+  }
+
+  @Test
+  @DisplayName("관리자 유저가 아닐 경우")
+  void validateAdminFail(){
+    //given
+    long userId = 1;
+    boolean flag = true;
+    UserServiceImpl userService = new UserServiceImpl(mockRepo,passwordEncoder);
+    given(mockRepo.validateAdminUser(userId)).willReturn(false);
+//    given(mockRepoCus.validateAdminUser(userId)).willReturn(true);
+//    given(mockRepoCusImpl.validateAdminUser(userId)).willReturn(true);
+
+    //when
+    try {
+      userService.validateAdminUser(userId);
+    }catch (UserNotFoundException e){
+      flag = false;
+    }
+    // then
+    assertFalse("해당 유저는 관리자가 아니에요",flag);
+  }
+}
+
