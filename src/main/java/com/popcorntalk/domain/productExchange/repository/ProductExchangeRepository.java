bbfb0ca53e16bd102a91254994fa22654101f9bd
pshistory <psh_a@naver.com>
2024-04-02 20:58:38 +0900

    remove(exchange) : 도메인명 변경으로 기존 파일들 삭제

--- src/main/java/com/popcorntalk/domain/productExchange/repository/ProductExchangeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.popcorntalk.domain.productExchange.repository;
-
-import com.popcorntalk.domain.productExchange.entity.ProductExchange;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductExchangeRepository extends JpaRepository<ProductExchange, Long> {
-
-}
-
