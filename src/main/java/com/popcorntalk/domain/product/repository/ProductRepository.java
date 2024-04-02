53ae82c077b571e645341f930ecbf4b944b6a8d0
parkjungsub <eses565@naver.com>
2024-04-02 09:57:18 +0900

    fix(product) : findAllByDeletionStatus 메소드 삭제

--- src/main/java/com/popcorntalk/domain/product/repository/ProductRepository.java
+++ src/main/java/com/popcorntalk/domain/product/repository/ProductRepository.java
@@ -1,11 +1,8 @@
 package com.popcorntalk.domain.product.repository;
 
 import com.popcorntalk.domain.product.entity.Product;
-import com.popcorntalk.global.entity.DeletionStatus;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
 
-    List<Product> findAllByDeletionStatus(DeletionStatus deletionStatus);
 }
