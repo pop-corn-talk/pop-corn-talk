cb482dc4ca6dd7794afbf821a004c6d4ad6f5b96
parkjungsub <eses565@naver.com>
2024-04-02 17:11:38 +0900

    fix(product) : 어노테이션 @Builder, @AllArgsConstructor 추가

--- src/main/java/com/popcorntalk/domain/product/dto/ProductReadResponseDto.java
+++ src/main/java/com/popcorntalk/domain/product/dto/ProductReadResponseDto.java
@@ -2,10 +2,12 @@ package com.popcorntalk.domain.product.dto;
 
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductReadResponseDto {
--- src/main/java/com/popcorntalk/domain/product/dto/ProductUpdateRequestDto.java
+++ src/main/java/com/popcorntalk/domain/product/dto/ProductUpdateRequestDto.java
@@ -2,11 +2,15 @@ package com.popcorntalk.domain.product.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class ProductUpdateRequestDto {
 
     @NotEmpty
