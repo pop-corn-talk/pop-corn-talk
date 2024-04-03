2d00afa16be36a968e7a5e9d061a3ea97a46a31d
onenyeong <a48142826@gmail.com>
2024-04-03 20:45:27 +0900

    fix(merge): COMMENT_NOT_FOUND 404처리

--- src/main/java/com/popcorntalk/global/exception/ErrorCode.java
+++ src/main/java/com/popcorntalk/global/exception/ErrorCode.java
@@ -11,7 +11,7 @@ public enum ErrorCode {
     INSUFFICIENT_POINT("400", "포인트가 부족합니다"),
     DUPLICATE_USER("400", "중복된 이메일 입니다."),
     USER_NOT_FOUND("400", "해당 유저를 찾을 수 없습니다."),
-    COMMENT_NOT_FOUND("400","해당 댓글을 찾을 수 없습니다.");
+    COMMENT_NOT_FOUND("404","해당 댓글을 찾을 수 없습니다.");
     private final String httpStatus;
     private final String msg;
 
