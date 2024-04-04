141a7b0718cec998d98c271a9bd426c79c59b246
parkjungsub <eses565@naver.com>
2024-04-04 11:44:37 +0900

    Rename(product): ProductReadResponseDto -> ProductGetResponseDto으로 수정

--- src/main/java/com/popcorntalk/domain/product/dto/ProductGetResponseDto.java
+++ src/main/java/com/popcorntalk/domain/product/dto/ProductGetResponseDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProductReadResponseDto {
+public class ProductGetResponseDto {
 
     private Long id;
     private String name;
