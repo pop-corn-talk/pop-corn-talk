ff15ac0088d0d1f57330087642200a2fe485925b
parkjungsub <eses565@naver.com>
2024-04-11 11:00:10 +0900

    feat(product): 상품의 갯수를 수정하는 메소드 추가

--- src/main/java/com/popcorntalk/domain/product/dto/ProductGetResponseDto.java
+++ src/main/java/com/popcorntalk/domain/product/dto/ProductGetResponseDto.java
@@ -22,4 +22,8 @@ public class ProductGetResponseDto {
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
 
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
 }
