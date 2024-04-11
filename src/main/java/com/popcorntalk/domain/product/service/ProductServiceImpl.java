229f2b464fef74fa4d43c795cc38aab150d3a9b7
parkjungsub <eses565@naver.com>
2024-04-11 11:12:05 +0900

    feat(product): 상품의 생성, 삭제, 수정 레디스에 저장하는 기능 추가

--- src/main/java/com/popcorntalk/domain/product/service/ProductServiceImpl.java
+++ src/main/java/com/popcorntalk/domain/product/service/ProductServiceImpl.java
@@ -13,6 +13,8 @@ import com.popcorntalk.global.exception.customException.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,35 +22,47 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class ProductServiceImpl implements ProductService {
 
-    private final ProductRepository productRepository;
+    private static final String HASH_KEY = "product";
     private final UserService userService;
+    private final ProductRepository productRepository;
+
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Override
     @Transactional
     public void createProduct(ProductCreateRequestDto productCreateRequestDto, Long userId) {
         userService.validateAdminUser(userId);
+        HashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
         Product product = Product.createOf(productCreateRequestDto);
+        Product svaedProduct = productRepository.save(product);
 
-        productRepository.save(product);
+        hashOperations.put(HASH_KEY, String.valueOf(svaedProduct.getId()),
+            String.valueOf(svaedProduct.getAmount()));
     }
 
     @Override
     @Transactional
     public void deleteProduct(Long productId, Long userId) {
+        HashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
         userService.validateAdminUser(userId);
         Product productDelete = getProduct(productId);
-
         productDelete.softDelete();
+
+        hashOperations.delete(HASH_KEY, String.valueOf(productDelete.getId()));
     }
 
     @Override
     @Transactional
     public void updateProduct(Long productId, ProductUpdateRequestDto productUpdateRequestDto,
         Long userId) {
+        HashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
         userService.validateAdminUser(userId);
         Product productUpdate = getProduct(productId);
-
         productUpdate.update(productUpdateRequestDto);
+
+        hashOperations.delete(HASH_KEY, String.valueOf(productUpdate.getId()));
+        hashOperations.put(HASH_KEY, String.valueOf(productUpdate.getId()),
+            String.valueOf(productUpdate.getAmount()));
     }
 
     @Override
@@ -63,6 +77,9 @@ public class ProductServiceImpl implements ProductService {
             .orElseThrow(() -> new NotFoundException(NOT_FOUND));
         if (product.getDeletionStatus() == DeletionStatus.Y) {
             throw new IllegalArgumentException("삭제된 상품입니다.");
+        }
+        if (product.getAmount() == 0) {
+            throw new IllegalArgumentException("상품의 수량이 없습니다.");
         }
         return product;
     }
