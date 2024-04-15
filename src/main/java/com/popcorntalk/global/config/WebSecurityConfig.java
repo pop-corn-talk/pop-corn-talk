b497f22a0162f27106d49fa48b9c5fb3e713365d
harrisbang2 <harrisbang2@gmail.com>
2024-04-15 10:46:30 +0900

    feat(front) : 프런트 엔드 연결/cors 해결

--- src/main/java/com/popcorntalk/global/config/WebSecurityConfig.java
+++ src/main/java/com/popcorntalk/global/config/WebSecurityConfig.java
@@ -16,11 +16,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class WebSecurityConfig {
+public class WebSecurityConfig implements WebMvcConfigurer {
 
     private final JwtUtil jwtUtil;
     private final AuthenticationConfiguration authenticationConfiguration;
@@ -62,6 +64,7 @@ public class WebSecurityConfig {
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
                 .permitAll() // resources 접근 허용 설정
                 .requestMatchers("/**").permitAll() // 메인 페이지 요청 허가
+                .requestMatchers("/posts/**").permitAll()
                 .requestMatchers("/users/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
                 .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
@@ -71,5 +74,16 @@ public class WebSecurityConfig {
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 요청을 받을 엔드포인트를 지정합니다.
+            .allowedOriginPatterns("*") // 허용할 origin을 설정합니다.
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드를 설정합니다.
+            .allowCredentials(true)
+            .allowedHeaders("Content-Type", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Expose-Headers") // 허용할 헤더를 설정합니다.
+            .exposedHeaders("Authorization");
     }
 }
