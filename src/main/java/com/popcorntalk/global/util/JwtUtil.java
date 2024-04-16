dade2883997e79c8a9419578cf4fe701e83afd4f
harrisbang2 <harrisbang2@gmail.com>
2024-04-16 18:04:11 +0900

    fix(user) : 기존 BEARER_PREFIX 안 붙어 오는 오류 해결

--- src/main/java/com/popcorntalk/global/util/JwtUtil.java
+++ src/main/java/com/popcorntalk/global/util/JwtUtil.java
@@ -76,6 +76,9 @@ public class JwtUtil {
             log.info("새로운 access 토큰 으로 진행");
             refreshToken.update(accessToken);
         }
+        else {
+            accessToken = BEARER_PREFIX + accessToken;
+        }
 
         redisUtil.set(redisKeys, refreshToken, (int) REFRESH_TOKEN_TIME);
         log.info("기존 refresh 토큰 으로 진행");
