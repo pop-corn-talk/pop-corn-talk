45588356fcd89f714b0f371cb544256c8ed3bb39
harrisbang2 <harrisbang2@gmail.com>
2024-04-18 22:29:14 +0900

    feat(log) : log test 적용 testlog4 미적용 테스트

--- src/test/java/com/popcorntalk/log/LogSpeedTest.java
+++ src/test/java/com/popcorntalk/log/LogSpeedTest.java
@@ -1,33 +1,48 @@
 package com.popcorntalk.log;
 
+import com.popcorntalk.domain.user.controller.UserController;
+import com.popcorntalk.domain.user.service.UserService;
+import com.popcorntalk.domain.user.service.UserServiceImpl;
+import com.popcorntalk.global.testlog.TestLogService;
+import com.popcorntalk.mockData.MockData;
 import java.sql.Time;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.slf4j.Log4jLogger;
+//import org.apache.logging.slf4j.Log4jLogger;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class LogSpeedTest extends TimeManager {
+public class LogSpeedTest extends TimeManager{
+
+
     // log4j 로거
     private static final Logger log4jLogger = LogManager.getLogger(LogSpeedTest.class);
 
+    private TestLogService testLogService = new TestLogService();
     @Test
-    @DisplayName("log4jLogger 테스트 번호 : 1")
-    void logback(){
-        for (int i = 0; i < 2000; i++) {
-            log4jLogger.debug("디버그 로그 메시지");
-            log4jLogger.info("정보 로그 메시지");
-            log4jLogger.warn("경고 로그 메시지");
-            log4jLogger.error("에러 로그 메시지");
-            log4jLogger.fatal("치명적 에러 로그 메시지");
-        }
-    }
-    @Test
-    @DisplayName("log4jLogger 테스트 번호 : 2")
-    void log4jLogger(){
+    @DisplayName("defaultLogTest 테스트 번호 : 1")
+    void defaultLogTest(){
+        testLogService.testNormalLogger();
     }
 
+    @Test
+    @DisplayName("defaultLogTest 테스트 번호 : 1")
+    void log4jLogTest(){
+        testLogService.testlog4jLogger();
+    }
+
+    @Test
+    @DisplayName("defaultLogTest 테스트 번호 : 2")
+    void defaultLogTest2(){
+        testLogService.testNormalLogger();
+    }
+    @Test
+    @DisplayName("defaultLogTest 테스트 번호 : 2")
+    void log4jLogTest2(){
+        testLogService.testlog4jLogger();
+    }
 }
