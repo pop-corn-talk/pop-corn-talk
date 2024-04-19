7cad465220ee87f6df0011039b0840791dad9539
harrisbang2 <harrisbang2@gmail.com>
2024-04-19 16:11:33 +0900

    refactor(log) : log4j2 동기화로 롤백

--- src/test/java/com/popcorntalk/log/LogSpeedTest.java
+++ src/test/java/com/popcorntalk/log/LogSpeedTest.java
@@ -1,18 +1,8 @@
 package com.popcorntalk.log;
 
-import com.popcorntalk.domain.user.controller.UserController;
-import com.popcorntalk.domain.user.service.UserService;
-import com.popcorntalk.domain.user.service.UserServiceImpl;
 import com.popcorntalk.global.testlog.TestLogService;
-import com.popcorntalk.mockData.MockData;
-import java.sql.Time;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-//import org.apache.logging.slf4j.Log4jLogger;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -20,13 +10,11 @@ public class LogSpeedTest extends TimeManager{
 
 
     // log4j 로거
-    private static final Logger log4jLogger = LogManager.getLogger(LogSpeedTest.class);
 
     private TestLogService testLogService = new TestLogService();
     @Test
     @DisplayName("로그테스트 테스트 번호 : 1")
     void defaultLogTest(){
-        testLogService.testNormalLogger();
+            testLogService.testNormalLogger();
     }
-
 }
