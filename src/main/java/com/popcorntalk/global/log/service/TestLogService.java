53c11cdd1173c77cf815d3b441bdd38d79e9f2e1
harrisbang2 <harrisbang2@gmail.com>
2024-04-22 17:51:04 +0900

    refactor(log) : 로그 key 값에 host ip address 추가

--- src/main/java/com/popcorntalk/global/log/service/TestLogService.java
+++ src/main/java/com/popcorntalk/global/log/service/TestLogService.java
@@ -6,12 +6,12 @@ import org.springframework.stereotype.Service;
 @Log4j2
 public class TestLogService {
     public void testLogger(){
-        for (int i = 0; i < 2000; i++) {
+        for (int i = 0; i < 5000; i++) {
             log.debug("디버그 로그 메시지");
             log.info("정보 로그 메시지");
             log.warn("경고 로그 메시지");
             log.error("에러 로그 메시지");
-            log.error("치명적 에러 로그 메시지");
+            log.fatal("치명적 에러 로그 메시지");
         }
     }
 
--- src/test/java/com/popcorntalk/log/LogTest.java
+++ src/test/java/com/popcorntalk/log/LogTest.java
@@ -2,16 +2,17 @@ package com.popcorntalk.log;
 
 import com.popcorntalk.global.log.service.LogService;
 import com.popcorntalk.global.log.service.TestLogService;
+import java.net.UnknownHostException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class LogTest extends TimeManager{
+public class LogTest extends TimeManager {
 
     @Autowired
-    private LogService LogService;
+    private LogService logService;
 
     private final TestLogService testLogService = new TestLogService();
 
@@ -19,12 +20,12 @@ public class LogTest extends TimeManager{
     @DisplayName("로그테스트 테스트 번호 : 1")
     void defaultLogTest(){
         // ci test 하면 실행되서 임시 주석
-        // testLogService.testLogger();
+        testLogService.testLogger();
     }
     @Test
     @DisplayName("로그 파일 가져오기/  로그 bucket 에 올리기")
-    void getFiles(){
+    void getFiles() throws UnknownHostException {
         // ci test 하면 실행되서 임시 주석
-        // LogService.uploadLogs();
+        logService.uploadLogs();
     }
 }
